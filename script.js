2536947bbcc004d069788508753d245023de4931
vijay kumar chaurasiya <118093154+vijay-kc@users.noreply.github.com>
2023-12-28 23:08:38 +0000

    add more functionality 1. more than one level 2. changing ui

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const playBoard = document.querySelector(".play-board");
 const scoreElement = document.querySelector(".score");
 const highScoreElement = document.querySelector(".high-score");
 const controls = document.querySelectorAll(".controls i");
-
+const lvl = document.querySelector(".level");
 let gameOver = false;
 let foodX, foodY;
 let snakeX = 5, snakeY = 5;
@@ -11,22 +11,25 @@ let snakeBody = [];
 let setIntervalId;
 let score = 0;
 
-
+// local storage of the game in the present tab
 let highScore = localStorage.getItem("high-score") || 0;
 highScoreElement.innerText = `High Score: ${highScore}`;
 
+// randomly generate a position of the food
 const updateFoodPosition = () => {
 
     foodX = Math.floor(Math.random() * 30) + 1;
     foodY = Math.floor(Math.random() * 30) + 1;
 }
 
+// when game is over clear && restart score
 const handleGameOver = () => {
     clearInterval(setIntervalId);
     alert("Game Over!! Press enter to replay.");
     location.reload();
 }
 
+// change the direction of snack
 const changeDirection = e => {
     if(e.key === "ArrowUp" && velocityY != 1) {
         velocityX = 0;
@@ -43,8 +46,12 @@ const changeDirection = e => {
     }
 }
 
+
+//controllers
 controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.dataset.key })));
 
+
+//running game
 const initGame = () => {
     if(gameOver) return handleGameOver();
     let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
@@ -80,6 +87,25 @@ const initGame = () => {
     playBoard.innerHTML = html;
 }
 
+var output = 1;
+var speed;
+function myFunction(){
+    selectElement = document.querySelector('#sel');
+    out =  selectElement.options[selectElement.selectedIndex].value;
+
+    location.reload();
+    
+  localStorage.setItem("output", out);
+  
+}
+output = localStorage.getItem("output");
+lvl.innerText = `Level: ${output}`;
+console.log(output);
+speed = (5-output)*50;
+    
 updateFoodPosition();
-setIntervalId = setInterval(initGame, 100);
+setIntervalId = setInterval(initGame, speed );
 document.addEventListener("keyup", changeDirection);
+
+
+
